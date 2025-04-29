5696b65c5eab2d6b197df8a263b4dae0dea20fbc
p209_stud04 <p209_stud04@domena211.local>
2025-04-29 11:03:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/menu_wysuwane/Toolbar.java
@@ -0,0 +1,21 @@
+package com.example.menu_wysuwane;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class Toolbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.toolbar);
+
+    }
+}
